54bd90a07132482a758d2d3e326336c202765ffa
brettswift <brettswift@gmail.com>
2018-09-09 21:55:00 -0600

    This commit needs to be broken down

--- cumulus/types/codebuild/buildaction.py
+++ cumulus/types/codebuild/buildaction.py
@@ -16,6 +16,36 @@ class SourceS3Action(troposphere.codepipeline.Actions):
             )
         self.RunOrder = "1"
 
+class LambdaAction(troposphere.codepipeline.Actions):
+    """
+        This class doesn't do much except set the ActionType to reduce code clutter
+    """
+    def __init__(self, **kwargs):
+        super(LambdaAction, self).__init__(**kwargs)
+
+        self.ActionTypeId = troposphere.codepipeline.ActionTypeId(
+                Category="Invoke",
+                Owner="AWS",
+                Version="1",
+                Provider='Lambda',
+            )
+        self.RunOrder = "1"
+
+class SourceCodeCommitAction(troposphere.codepipeline.Actions):
+    """
+        This class doesn't do much except set the ActionType to reduce code clutter
+    """
+    def __init__(self, **kwargs):
+        super(SourceCodeCommitAction, self).__init__(**kwargs)
+
+        self.ActionTypeId = troposphere.codepipeline.ActionTypeId(
+                Category="Source",
+                Owner="AWS",
+                Version="1",
+                Provider="CodeCommit",
+            )
+        self.RunOrder = "1"
+
 
 class CodeBuildAction(troposphere.codepipeline.Actions):
     """
